dcb7612921b848dd3e75f4f5b8b96940ef8a7178
Stefan Katic <katicstefan.coder@gmail.com>
2021-07-19 08:11:27 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/depsylsservice/DepsylsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.depsylsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepsylsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepsylsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/depsylsservice/DepsylsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.depsylsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepsylsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
